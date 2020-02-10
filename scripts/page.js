c1d51816eff81999ccfd29399451b85a83a76495
Estrella-BF <gracielabenitesf@gmail.com>
2020-02-09 23:48:52 -0500

    feature: agregando blog vista

--- scripts/page.js
+++ scripts/page.js
@@ -1,6 +1,6 @@
   // Navigation
-  const navigationTabsName = ['aboutMeBtnDesktop', 'whatIDoBtnDesktop', 'therapyDesktop'];
-  const blocksName = ['aboutMe', 'whatIDo', 'therapy'];
+  const navigationTabsName = ['aboutMeBtnDesktop', 'whatIDoBtnDesktop', 'therapyDesktop', 'testimonialsDesktop', 'blogDesktop'];
+  const blocksName = ['aboutMe', 'whatIDo', 'therapy', 'testimonials', 'blog'];
   
   const aboutMeButton = document.getElementById('aboutMeBtnDesktop');
   aboutMeButton.setAttribute("class", "active");
@@ -8,6 +8,8 @@
   const aboutMeBtnDesktop = document.getElementById('aboutMeBtnDesktop');
   const whatIDoBtnDesktop = document.getElementById('whatIDoBtnDesktop');
   const therapyDesktop = document.getElementById('therapyDesktop');
+  const testimonialsDesktop = document.getElementById('testimonialsDesktop');
+  const blogDesktop = document.getElementById('blogDesktop');
 
   aboutMeBtnDesktop.addEventListener('click', () => {
     this.activateClass('aboutMeBtnDesktop');
@@ -19,6 +21,14 @@
   
   therapyDesktop.addEventListener('click', () => {
     this.activateClass('therapyDesktop');
+  });
+  
+  testimonialsDesktop.addEventListener('click', () => {
+    this.activateClass('testimonialsDesktop');
+  });
+  
+  blogDesktop.addEventListener('click', () => {
+    this.activateClass('blogDesktop');
   });
 
   function activateClass(value) {
