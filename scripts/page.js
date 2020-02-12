ae65f5af5cdd872a05f4308f0eeee16826855c9a
Estrella-BF <gracielabenitesf@gmail.com>
2020-02-12 01:38:25 -0500

    feature: adding modals

--- scripts/page.js
+++ scripts/page.js
@@ -39,7 +39,7 @@
 
 
   this.activateClass('aboutMeBtnDesktop');
-  
+
   aboutMeBtnDesktop.addEventListener('click', () => {
     this.activateClass('aboutMeBtnDesktop');
   });
@@ -114,6 +114,7 @@
         block.setAttribute("class", "hidden");
       }
     });
+    window.scrollTo(0, 0)
     buttonMobileMenu();
   }
   
@@ -135,3 +136,101 @@
     }
 
   }
+
+  // navigation
+  var current_page = 1;
+var records_per_page = 10;
+
+var objJson = [
+    { adName: "1.- ACEPTAR, CAMBIAR Y FLUIR"},
+    { adName: "2.- ANSIEDAD, APRENDIENDO A VIVIR EN PAZ"},
+    { adName: "3.- APRENDIENDO LA COMPASIÓN PARA LA LIBERACIÓN"},
+    { adName: "4.- BORDERLINE: TRASTORNO NARCISISTA DE LA PERSONALIDAD"},
+    { adName: "5.- CAMBIAMOS SI ACEPTAMOS"},
+    { adName: '6.- CONECTANDO CON LAS EMOCIONES "NEGATIVAS"'},
+    { adName: "7.- DEPENDENCIA EMOCIONAL HACIA LA PAREJA"},
+    { adName: "8.- DEPRESIÓN, SALIENDO DE LA TRISTEZA PROFUNDA"},
+    { adName: "9.- DISMORFIA: CUANDO TE VES FEO SIN SERLO"},
+    { adName: "10.- EL ARTE DE LA PSICOTERAPIA"},
+    { adName: "11.- EMPATÍA, CONECTANDO CON EL OTRO "},
+    { adName: "12.- ÉTICA Y PROFESIONALISMO DEL PSICÓLOGO "},
+    { adName: "13.- HÁBITOS DE VIDA, DESDE LA PSICOLOGÍA"},
+    { adName: "14.- HACER CONSCIENTE LO INCONSCIENTE ES PARTE DEL PROCESO TERAPÉUTICO"},
+    { adName: "15.- HAZTE LA PREGUNTA PARA QUÉ Y NO POR QUÉ"},
+    { adName: "16.- LA TECNOLOGÍA Y LAS RELACIONES INTER-PERSONALES"},
+    { adName: "17.- LOS TRASTORNOS MENTALES MEJORAN Y SE CURAN"},
+    { adName: "18.- NADA PERSONAL"},
+    { adName: "19.- NO TE COMPARES CON LOS DEMÁS"},
+    { adName: "20.- PARA LOS QUE NO ENCAJAMOS DEL TODO Y DE VEZ EN CUANDO NOS LLAMAN LOCOS"},
+    { adName: "21.- PARA SER PSICÓLOGO CLÍNICO ES CLAVE HABER SIDO PACIENTE"},
+    { adName: "22.- PERDONAR NO ES OLVIDAR"},
+    { adName: "23.- ¿PODEMOS CAMBIAR?"},
+    { adName: "24.- QUIERO TENER Y MANTENER UNA RELACIÓN DE PAREJA PERO…"},
+    { adName: "25.- REESTRUCTURANDO PENSAMIENTOS, REGULANDO EMOCIONES"},
+    { adName: "26.- RELACIÓN DE PAREJA, COMO PODRÍA FUNCIONAR"},
+    { adName: "27.- RESILIENCIA, CONVIRTIENDO LA ADVERSIDAD EN UNA OPORTUNIDAD"},
+    { adName: "28.- SÉ EL ARQUITECTO DE TU VIDA "},
+    { adName: "29.- SILENCIA LA MENTE "},
+    { adName: "30.- SI QUIERES UNA RELACIÓN DE PAREJA ESTABLE NO TE ACUESTES TAN RÁPIDO"},
+    { adName: "31.- TRASCENDIENDO A LA CONSCIENCIA "},
+    { adName: "32.- TRASTORNO NARCISISTA DE LA PERSONALIDAD"},
+    { adName: "33.- TRASTORNO HISTRIÓNICO DE LA PERSONALIDAD"},
+]; // Can be obtained from another source, such as your objJson variable
+
+function prevPage()
+{
+    if (current_page > 1) {
+        current_page--;
+        changePage(current_page);
+    }
+}
+
+function nextPage()
+{
+    if (current_page < numPages()) {
+        current_page++;
+        changePage(current_page);
+    }
+}
+    
+function changePage(page)
+{
+    var btn_next = document.getElementById("btn_next");
+    var btn_prev = document.getElementById("btn_prev");
+    var listing_table = document.getElementById("listingTable");
+    var page_span = document.getElementById("page");
+ 
+    // Validate page
+    if (page < 1) page = 1;
+    if (page > numPages()) page = numPages();
+
+    listing_table.innerHTML = "";
+
+    for (var i = (page-1) * records_per_page; i < (page * records_per_page); i++) {
+        listing_table.innerHTML += 
+        "<li data-toggle='modal' data-target='#modal-" + i + "'>" + objJson[i].adName + "</li>" + "<br>";
+    }
+    
+    page_span.innerHTML = page;
+
+    if (page == 1) {
+      btn_prev.parentElement.setAttribute("class", 'navigation__item disabled-pagination');
+    } else {
+      btn_prev.parentElement.setAttribute("class", 'navigation__item');
+    }
+
+    if (page == numPages()) {
+      btn_next.parentElement.setAttribute("class", 'navigation__item disabled-pagination');
+    } else {
+      btn_next.parentElement.setAttribute("class", 'navigation__item');
+    }
+}
+
+function numPages()
+{
+    return Math.ceil(objJson.length / records_per_page);
+}
+
+window.onload = function() {
+    changePage(1);
+};
